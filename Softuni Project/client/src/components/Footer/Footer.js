63910795da12ff6a63073743fd406f8b62450570
Stan4Bulgaria <117578267+Stan4Bulgaria@users.noreply.github.com>
2023-04-19 10:08:35 +0300

    Delete Softuni Project/client directory

--- Softuni Project/client/src/components/Footer/Footer.js
+++
@@ -1,8 +0,0 @@
-import styles from './Footer.module.css';
-export const Footer = () => {
-    return (
-        <footer className={styles["footer"]}>
-            <p>Copyright © 2023 Your Website</p>
-        </footer>
-    )
-}
